709f351efeae2968b5ff3333a94b023c23ccdb4d
mikadoSMN <misuko2020@outlook.jp>
2024-05-14 22:52:45 +0900

    MyProgramで作成したUIを移す

---
+++ Assets/ForStart.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class ForStart : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    // Update is called once per frame
+    public void StartButton()
+    {
+        Debug.Log("ForStart");
+        SceneManager.LoadScene("Action");
+    }
+}
+
