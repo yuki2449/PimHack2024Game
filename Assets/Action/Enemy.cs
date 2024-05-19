fad0b87d306191141e79852551fe2594672395ee
yuki2449 <158599254+yuki2449@users.noreply.github.com>
2024-05-19 17:16:34 +0900

    敵が物体に近づくようにしました

--- Assets/Action/Enemy.cs
+++ Assets/Action/Enemy.cs
@@ -7,10 +7,29 @@ namespace ENEMY
 
     public class Enemy : MonoBehaviour
     {
-        [SerializeField] public int HP;         //HP
+        [SerializeField] public int HP;          //HP
+        private GameObject DefenseObject;       //でかミカンオブジェクトを定義
+        private Rigidbody rb; //物理演算コンポーネント
+        [SerializeField] private float enemy_speed; //スピードの定義
+        [SerializeField] private Vector3 enemy_move_direction;     //enemy移動の向き
+        [SerializeField] private Vector3 enemy_Angle;         //enemyの角度
+
+
+        void Start()
+        {
+            DefenseObject = GameObject.FindWithTag("Defence");
+
+            rb = this.gameObject.GetComponent<Rigidbody>(); //Rigidbodyをコンポーネントに追加
+        }
+
 
         void Update()
         {
+            transform.position = Vector3.MoveTowards(
+                transform.position,
+                DefenseObject.transform.position,
+                enemy_speed * Time.deltaTime);
+
             if (HP <= 0)                        //HPが０になった時消える
             {
                 Destroy(gameObject);
@@ -21,5 +40,12 @@ namespace ENEMY
             HP -= damage;
         }
 
+        private void OnCollisionEnter(Collision collision)
+        {
+            if (collision.gameObject.tag == "Defence")
+            {
+                Destroy(this.gameObject);
+            }
+        }
     }
 }
