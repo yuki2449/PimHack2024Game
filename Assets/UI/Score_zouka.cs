e4ef6fd68823a820e05640a50a4ac995964aee6b
TanimizuHaru <167054954+TanimizuHaru@users.noreply.github.com>
2024-05-31 21:59:51 +0900

    スコアの増加の修正

--- Assets/UI/Score_zouka.cs
+++ Assets/UI/Score_zouka.cs
@@ -10,10 +10,6 @@ public class Score_zouka : MonoBehaviour
         // 既存のオブジェクトから ScoreManager コンポーネントを取得していると仮定します
         scoreManager = FindObjectOfType<ScoreManager>();
 
-        // ScoreManager クラスのプロパティにアクセスして値を変更する例
-        if (scoreManager != null)
-        {
-            scoreManager.score_num += 100;
-        }
+        scoreManager.score_num += 100;
     }
 }
